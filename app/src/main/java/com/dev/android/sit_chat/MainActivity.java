26f60c30db138ac3191acbee6bd17d401a9c6e1a
Decode <Decode1@example.com>
2023-03-07 21:57:27 +0530

    Menu added to the ActionBar and logout credential added

--- app/src/main/java/com/dev/android/sit_chat/MainActivity.java
+++ app/src/main/java/com/dev/android/sit_chat/MainActivity.java
@@ -1,14 +1,59 @@
 package com.dev.android.sit_chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.dev.android.sit_chat.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth=FirebaseAuth.getInstance();
+
+    }
+
+
+    //Set Menu to the Action Bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //On Item Selected
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.groupChat:
+                Toast.makeText(this, "Group Chat", Toast.LENGTH_SHORT).show();
+                 break;
+            case R.id.logout:
+                 mAuth.signOut();
+                Intent intent=new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+                finish();
+                 break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
